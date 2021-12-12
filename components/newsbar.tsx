a29a93f3ca41a26891a7d2da932359d2eab71150
Taric <taric.andrade@gmail.com>
2021-12-12 00:16:35 -0800

    Update newsbar.tsx
    Add github link

--- components/newsbar.tsx
+++ components/newsbar.tsx
@@ -13,7 +13,7 @@ export default function News() {
           </div>
           <div className="order-1 mt-2 flex-shrink-0 w-full sm:order-1 sm:mt-0 sm:w-auto">
             <a
-              href="#"
+              href="https://github.com/taricsa/openportfolio"
               className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-blue-600 bg-white hover:bg-indigo-50"
             >
               Learn more
