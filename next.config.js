61964ed71c737578a34a904e6a82593f30e017df
Taric <taricsa@users.noreply.github.com>
2021-12-12 14:49:12 -0800

    Create next.config.js
    Add access to external images.

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      domains: ['media-exp1.licdn.com', 'miro.medium.com', 'images.unsplash.com'],
+    }
+  }
