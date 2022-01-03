b08c692f359e5d821ee0f98a445615ae425ce0b8
Taric <taricsa@users.noreply.github.com>
2022-01-02 22:40:53 -0800

    Adding Modal-Video
    Adding Modal for video on the profile component by creating the watchme component. The video is a Youtube one embedded.

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import ProfileImage from '../assets/profile-pic.png'
+import WatchMe from './watchme';
 
 // Replace the file profile-pic.png with your profile-pic
 // I was too lazy to build the const for all the inputs of this component, but they are easy to find and replace it. I will improve it later.
@@ -60,12 +61,7 @@ export default function ProfileHero() {
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div className="py-6 px-3 mt-32 sm:mt-0">
-                      <button
-                        className="bg-blue-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1"
-                        type="button"
-                        style={{ transition: "all .15s ease" }} >
-                        Video
-                      </button>
+                      < WatchMe />
                     </div>
                   </div>
                   <div className="w-full lg:w-4/12 px-4 lg:order-1">
